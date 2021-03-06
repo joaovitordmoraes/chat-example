e057335ee1f2fa8ad2d1585ff79fbe36c2bf87fd
Joao <joaovitordmoraes@gmail.com>
2021-03-06 13:45:15 -0300

    :bug: Fix formik bug

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -3,30 +3,34 @@ import { Formik, Form, Field } from 'formik';
 
 function Chat({ initialValues, handleSubmit }) {
   return (
-    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-      {() => {
-        <Form>
-          <div className="form-group">
-            <Field name="user" placeholder="User" type="text" />
-          </div>
+    <div>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        {() => {
+          return (
+            <Form>
+              <div className="form-group">
+                <Field name="user" placeholder="User" type="text" />
+              </div>
 
-          <div className="form-group">
-            <Field name="city" placeholder="Cidade" type="text" />
-            <Field name="state" placeholder="Estado" type="text" />
-          </div>
+              <div className="form-group">
+                <Field name="city" placeholder="Cidade" type="text" />
+                <Field name="state" placeholder="Estado" type="text" />
+              </div>
 
-          <div className="form-group">
-            <Field name="birth" placeholder="Digite sua data de nascimento" type="date" />
-          </div>
+              <div className="form-group">
+                <Field name="birth" placeholder="Digite sua data de nascimento" type="date" />
+              </div>
 
-          <div className="form-group">
-            <Field name="email" placeholder="Digite seu e-mail" type="email" />
-          </div>
+              <div className="form-group">
+                <Field name="email" placeholder="Digite seu e-mail" type="email" />
+              </div>
 
-          <button type="submit">Salvar</button>
-        </Form>
-      }}
-    </Formik>
+              <button type="submit">Salvar</button>
+            </Form>
+          );
+        }}
+      </Formik>
+    </div>
   );
 }
 
